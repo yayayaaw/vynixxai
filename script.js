df6347fbc81314855e3c4fa6d88be94b5ecd408a
yayayaaw <karmayuga33@gmail.com>
2025-08-17 00:59:29 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('moneyButton').addEventListener('click', function() {
+    // Kode iklan Anda dimulai di sini
+    var adScript = document.createElement('script');
+    adScript.type = 'text/javascript';
+    adScript.src = 'https://openerclassify.com/i7zu49628x?key=95f53d9aa1565e5092e6fb3cf1fc1404';
+    document.body.appendChild(adScript);
+    // Kode iklan berakhir di sini
+});
